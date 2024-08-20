de39457cfdfe9ec213096822ccc7f57cc093277f
Pawan kumar <63099276+pawan941394@users.noreply.github.com>
2024-08-20 13:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,29 @@ with st.spinner("Downloading NLTK data..."):
 
 # Set Google API Key
 os.environ["GOOGLE_API_KEY"] = st.secrets['api_key']
-
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <a href="https://www.instagram.com/p_awan__kumar/" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" width="40" style="margin-right: 10px;">
+        </a>
+        <a href="https://www.linkedin.com/in/pawan941394/" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="40" style="margin-right: 10px;">
+        </a>
+        <a href="https://www.youtube.com/channel/UClgbj0iYh5mqY_81CMCw25Q/" target="_blank">
+            <img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" width="40" style="margin-right: 10px;">
+        </a>
+        <a href="https://github.com/pawan941394" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" width="40" style="margin-right: 10px;">
+        </a>
+   <a href="https://wa.me/919057714590" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="40" style="margin-right: 10px;">
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+    
 st.title("Gen AI: News Research Tool 📈")
 st.sidebar.title("Enter Your URLs Here")
 
